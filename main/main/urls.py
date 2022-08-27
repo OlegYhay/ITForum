474eb00a0c7a5e43efda0dd44315b99617e882bd
OlegYhay <108109792+OlegYhay@users.noreply.github.com>
2022-08-27 15:29:04 +0500

    init

--- main/main/urls.py
+++ main/main/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from .settings import MEDIA_URL, MEDIA_ROOT
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+                  path('admin/', admin.site.urls),
+              ] + static(MEDIA_URL, document_root=MEDIA_ROOT)
