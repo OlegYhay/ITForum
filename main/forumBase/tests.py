acd3ed07adbffffcb65169b239c16f1084ce7388
Oleg <jadk.fre@Mail.ru>
2022-09-08 16:42:38 +0500

    -all

--- main/forumBase/tests.py
+++ main/forumBase/tests.py
@@ -54,9 +54,10 @@ class TestHomePage(TestCase):
 
     def test_get_topic_detail(self):
         client = self.client
+        client.force_login(self.User)
         url = reverse('topic', kwargs={'group': self.Category.pk, 'pk': self.Topic1.id})
         result = client.get(url)
         self.assertEqual(result.status_code, 200)
-        self.assertContains(result,'Как вывести данные python в командную строку?')
+        self.assertContains(result, 'Как вывести данные python в командную строку?')
         self.assertTemplateUsed('pages/Topic.html')
-        print(result)
+        print(result.request)
